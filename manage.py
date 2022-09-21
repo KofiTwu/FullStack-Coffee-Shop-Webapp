6bdc2f3c6cce58cc0993d2b3e3dc0452288f8f7f
KofiTwu <69398632+KofiTwu@users.noreply.github.com>
2022-09-20 23:11:02 -0400

    Add files via upload

---
+++ manage.py
@@ -0,0 +1,14 @@
+from flask_script import Manager
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_migrate import Migrate, MigrateCommand
+
+from app import app
+from models import db, Movie, Actor
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
